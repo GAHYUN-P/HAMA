ccd95eec0c024038c6e4b7d22b318216b4bb98c7
GAHYUN-P <gahyun94526@gmail.com>
2022-03-22 02:24:17 +0900

    배너 이벤트 라우팅, home 커버 재설정

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useSelector,useDispatch } from 'react-redux';
+import { utilActions } from '../redux/modules/util';
 
 import Header from '../components/Header';
 import RankList from '../components/RankList';
@@ -13,16 +15,20 @@ import IntroPop from '../components/IntroPop';
 
 const Home = (props) => {
 
+  const dispatch = useDispatch();
+
   // 팝업창 키기/종료
   // true가 기본 상태
-  const [popupOpen, setPopupOpen] = React.useState(true);
+  // const [popupOpen, setPopupOpen] = React.useState(true);
+
+  const popupOpen = useSelector((state) => state.util.popupOpen);
 
   // 팝업창 키기/끄기 함수
   // const openPopup = () => {
   //   setPopupOpen(true);
   // };
   const closePopup = () => {
-    setPopupOpen(false);
+    dispatch(utilActions.setPopupOpen(false));
   };
 
 
