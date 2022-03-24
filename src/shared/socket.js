510af611468fd8128711da5d260163e20050a1e2
GAHYUN-P <gahyun94526@gmail.com>
2022-03-24 11:17:23 +0900

    서버 변경

--- src/shared/socket.js
+++ src/shared/socket.js
@@ -5,7 +5,7 @@ import { alarmActions } from '../redux/modules/alarm';
 
 import { getToken,getUserId } from '../shared/cookie';
 
-const sock = new SockJS('https://gongbuhyeyum.shop/ws-stomp');
+const sock = new SockJS('https://jumong.xyz/ws-stomp');
 const ws = Stomp.over(sock);
 const token = getToken();
 const userId = getUserId();
